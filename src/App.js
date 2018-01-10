d3c4b6988f14394e21c69269fcd100e37b99704c
five <five@iii.org.tw>
2018-01-10 16:16:37 +0800

    設定初始like值 當onClick被呼叫，like值+1

--- src/App.js
+++ src/App.js
@@ -6,12 +6,22 @@ import Counter from './counter.js';
 import ButtonLike from './buttonLike.js';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={likes:0};   
+    this.like =  this.like.bind(this);
+  }
+
+  like(){
+    console.log("+1");
+    this.setState({likes:this.state.likes+1});  
+  }
   render() {
     return (
       <div className="App">
         <Header/>
-        <Counter/>
-        <ButtonLike/>        
+        <Counter likes={this.state.likes}/>
+        <ButtonLike onClick={()=>this.like()}/>        
       </div>
     );
   }
