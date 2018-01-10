b93ba06c7921f96702033663290d2ee3f1e39053
five <five@iii.org.tw>
2018-01-10 16:07:48 +0800

    設定counter初始值與限制

--- src/counter.js
+++ src/counter.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-class Counter extends Component {
+import PropTypes from 'prop-types';
+
+
+const propTypes = {
+  likes:PropTypes.number
+}
+
+const defaultProps = {
+  likes:0
+}
+
+
+export default class Counter extends Component {
     render() {
       return (
         <div className="counter">
-          <p>Likes : 0</p>
+          <p>Likes : {this.props.likes}</p>
         </div>
       );
     }
   }
 
-  export default Counter;
+
+
+Counter.propTypes = propTypes;
+Counter.defaultProps = defaultProps;
