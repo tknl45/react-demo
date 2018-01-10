3ba96d400157e69c5fd200baf084c32f5cb8f4e9
five <five@iii.org.tw>
2018-01-10 08:54:40 +0800

    UI雛形

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>React Example</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="counter">
+          <p>Likes : 0</p>
+        </div>
+        <div className="button-like-counter">
+          <button className="bt">Like</button>
+        </div>        
       </div>
     );
   }
